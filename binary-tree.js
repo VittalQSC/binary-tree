f4423f07ab517ab61848b92441c84d003d96ca2a
VittalQSC <azrial96@rambler.ru>
2016-01-29 01:42:10 +0200

    adding insert

--- binary-tree.js
+++ binary-tree.js
@@ -6,7 +6,32 @@ class BinaryTree {
 	}
 
 	insert(data) {
+		if(this.root == null) {
+			this.root = new Node(data,null,null);
+			return;
+		}
 
+		var curNode = this.root;
+		while(true) {
+			if(curNode.data == data) {return;}
+			else if(curNode.data < data) {
+				if(!curNode.right) {
+					curNode.right = new Node(data,null,null);
+					return;
+				} else {
+					curNode = curNode.right;
+					continue;
+				}
+			} else if(curNode.data > data) {
+				if(!curNode.left) {
+					curNode.left = new Node(data,null,null);
+					return;
+				} else {
+					curNode = curNode.left;
+					continue;
+				}
+			}
+		}
 	}
 
 	contains(data) {
