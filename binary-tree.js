1e05acc8a17b09b5c20429c5cf44e8f26e011bfb
VittalQSC <azrial96@rambler.ru>
2016-01-29 14:46:44 +0200

    adding remove (tests still has mistake?), size and isEmpty

--- binary-tree.js
+++ binary-tree.js
@@ -59,17 +59,93 @@ class BinaryTree {
 		}
 	}
 
-	recursiveMin(root) {
-		if(!root.left) return root.data;
-		else return recursiveMin(root.left);
+	remove(data) {
+		if(!this.contains(data)) return;
+		if(!this.root.left && !this.root.right) {
+			this.root = null;
+			return;
+		}
+
+		var parent = null;
+		var curNode = this.root;
+		while(curNode.data != data) {
+			if(!curNode) return;
+			parent = curNode;
+			if(data < curNode.data) {
+				if(!curNode.left) return;
+				curNode = curNode.left;
+				continue;
+			} else if(data > curNode.data) {
+				if(!curNode.right) return;
+				curNode = curNode.right;
+				continue;
+			}
+		}
+
+		//find subtree min and remove leaf
+		var minData;
+		var subtree;
+		var subtreeParent = curNode;
+		if(curNode.left) {
+			subtree = curNode.left;
+
+			while(subtree.right) {
+				subtreeParent = subtree;
+				subtree = subtree.right;
+			}
+			minData = subtree.data;
+			if(subtreeParent.data != curNode.data) subtreeParent.right = subtree.left;
+			else subtreeParent.left = subtree.left;
+		 } else if(curNode.right) {
+			subtree = curNode.right;
+
+			while(subtree.left) {
+				subtreeParent = subtree;
+				subtree = subtree.left;
+			}
+			minData = subtree.data;
+			if(subtreeParent.data != curNode.data) subtreeParent.left = subtree.right;
+			else subtreeParent.right = subtree.right;
+		} else if(!curNode.left && !curNode.right){
+			if(parent) {
+				if(parent.left && parent.left.data == data) parent.left = null;
+				else if(parent.right && parent.right.data == data) parent.right = null;
+			}
+			return;
+		}
+
+		curNode.data = minData;
+
 	}
 
-	remove(data) {
+	getArr(root) {
+		var data = [];
+		if(!root) return data;
+		if(root.left){
+			data = data.concat(this.getArr(root.left));
+		}
+		data.push(root.data);
+		if(root.right){
+			data = data.concat(this.getArr(root.right));
+		}
+		return data;
+	}
 
+	getInnerArr(root) {
+		var data = [];
+		if(!root) return data;
+		data.push(root.data);
+		if(root.left){
+			data = data.concat(this.getArr(root.left));
+		}
+		if(root.right){
+			data = data.concat(this.getArr(root.right));
+		}
+		return data;
 	}
 
 	size() {
-
+		return this.getArr(this.root).length;
 	}
 
 	isEmpty() {
