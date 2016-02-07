8c2be86989cce38da352e3cb8e392c394a746cb1
VittalQSC <azrial96@rambler.ru>
2016-02-07 14:38:22 +0200

    making compact code

--- binary-tree.js
+++ binary-tree.js
@@ -5,6 +5,12 @@ class BinaryTree {
 		this.root = null;
 	}
 
+	static getSide(data, nodeData) {
+		if(data == nodeData) return undefined;
+		if(data < nodeData) return 'left';
+		else if(data > nodeData) return 'right';
+	}
+
 	insert(data) {
 		if(this.root == null) {
 			this.root = new Node(data,null,null);
@@ -12,24 +18,16 @@ class BinaryTree {
 		}
 
 		var curNode = this.root;
+		var side;
 		while(true) {
-			if(curNode.data == data) {return;}
-			else if(curNode.data < data) {
-				if(!curNode.right) {
-					curNode.right = new Node(data,null,null);
-					return;
-				} else {
-					curNode = curNode.right;
-					continue;
-				}
-			} else if(curNode.data > data) {
-				if(!curNode.left) {
-					curNode.left = new Node(data,null,null);
-					return;
-				} else {
-					curNode = curNode.left;
-					continue;
-				}
+			side = BinaryTree.getSide(data,curNode.data);
+
+			if(!side) return;
+			if(!curNode[side]) {
+				curNode[side] = new Node(data,null,null);
+				return;
+			} else {
+				curNode = curNode[side];
 			}
 		}
 	}
@@ -38,23 +36,13 @@ class BinaryTree {
 		if(!this.root) return false;
 
 		var curNode = this.root;
-
 		while(true) {
-			if(curNode.data == data) return true;
-			else if(curNode.data < data) {
-				if(!curNode.right) {
-					return false;
-				} else {
-					curNode = curNode.right;
-					continue;
-				}
-			} else if(curNode.data > data) {
-				if(!curNode.left) {
-					return false;
-				} else {
-					curNode = curNode.left;
-					continue;
-				}
+			var side = BinaryTree.getSide(data, curNode.data);
+
+			if(!side) return true;
+			if(!curNode[side]) return false;
+			else {
+				curNode = curNode[side];
 			}
 		}
 	}
@@ -65,67 +53,51 @@ class BinaryTree {
 			this.root = null;
 			return;
 		}
+		var side; // for search and removing
 
 		var parent = null;
 		var curNode = this.root;
 		while(curNode.data != data) {
 			if(!curNode) return;
 			parent = curNode;
-			if(data < curNode.data) {
-				if(!curNode.left) return;
-				curNode = curNode.left;
-			} else if(data > curNode.data) {
-				if(!curNode.right) return;
-				curNode = curNode.right;
-			}
+
+			side = BinaryTree.getSide(data,curNode.data);
+			if(!curNode[side]) return;
+			curNode = curNode[side];
 		}
 
 		//find subtree min and remove leaf
-		var minData;
+		var minData = Number.MIN_VALUE;
 		var subtree;
 		var subtreeParent = curNode;
 		if(curNode.left && curNode.right) {
-			if (curNode.left) {
-				subtree = curNode.left;
+			var oppositeSide =  {
+				right: 'left',
+				left: 'right'
+			};
+			if(curNode.left) side = 'left';
+			else if(curNode.right) side= 'right';
 
-				while (subtree.right) {
-					subtreeParent = subtree;
-					subtree = subtree.right;
-				}
-				minData = subtree.data;
-				if (subtreeParent.data != curNode.data) subtreeParent.right = subtree.left;
-				else subtreeParent.left = subtree.left;
-			} else if (curNode.right) {
-				subtree = curNode.right;
-
-				while (subtree.left) {
-					subtreeParent = subtree;
-					subtree = subtree.left;
-				}
-				minData = subtree.data;
-				if (subtreeParent.data != curNode.data) subtreeParent.left = subtree.right;
-				else subtreeParent.right = subtree.right;
+			subtree = curNode[side];
+			while(subtree[oppositeSide[side]]) {
+				subtreeParent = subtree;
+				subtree = subtree[oppositeSide[side]];
 			}
-		} else if(curNode.left) {
-			subtree = curNode.left;
+			minData = subtree.data;
+			if(subtreeParent.data != curNode.data) subtreeParent[oppositeSide[side]] = subtree[side];
+			else subtreeParent[side] = subtree[side];
+
+		} else {
+			if(curNode.left) subtree = curNode.left;
+			else if(curNode.right) subtree = curNode.right;
+				 else subtree = null;
 
 			if (parent) {
 				if (parent.left && parent.left.data == data) parent.left = subtree;
 				else if (parent.right && parent.right.data == data) parent.right = subtree;
 			}
-		} else if(curNode.right) {
-			subtree = curNode.right;
 
-			if (parent) {
-				if (parent.left && parent.left.data == data) parent.left = subtree;
-				else if (parent.right && parent.right.data == data) parent.right = subtree;
-			}
-		} else if (!curNode.left && !curNode.right) {
-			if (parent) {
-				if (parent.left && parent.left.data == data) parent.left = null;
-				else if (parent.right && parent.right.data == data) parent.right = null;
-			}
-			return;
+			if(!curNode.left && !curNode.right) return;
 		}
 
 		curNode.data = minData;
@@ -139,19 +111,6 @@ class BinaryTree {
 			data = data.concat(this.getArr(root.left));
 		}
 		data.push(root.data);
-		if(root.right){
-			data = data.concat(this.getArr(root.right));
-		}
-		return data;
-	}
-
-	getInnerArr(root) {
-		var data = [];
-		if(!root) return data;
-		data.push(root.data);
-		if(root.left){
-			data = data.concat(this.getArr(root.left));
-		}
 		if(root.right){
 			data = data.concat(this.getArr(root.right));
 		}
